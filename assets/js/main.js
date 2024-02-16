4d2442f112997246e216823b0ee5930d766517a5
StefDev1801 <stefano200118@gmail.com>
2024-02-16 16:21:52 +0100

    add Esercizio/2C

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,29 @@
 
 const number = '100'; //string
 
-console.log(number) //string?
+console.log(number)
 
 //Esercizio 2: Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
 
-const PI = '3.14'; //string
+const PI = 3.14; //number
 
-console.log( PI ) //string?
+console.log( PI )
 
 //Esercizio  2/B: Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
-
+/*
 PI = '3.1416'; //string
 
-console.log( PI ) 
+console.log( PI )   //Uncaught TypeError: Assignment to constant variable: questo il tipo di variabile non può essere cambiata dato che non è riassegnabile
 
-//Uncaught TypeError: Assignment to constant variable: questo il tipo di variabile non può essere cambiata dato che non è riassegnabile
+*/
+
+/*Esercizio 2/C 
+Dichiara una variabile chiamata radius e assegnale il valore 8.
+Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+Stampa il valore della variabile circle in console.
+*/
+const radius = 8 //number
+
+const circle = radius * 2 * PI //number
+
+console.log (circle);
