559e4e6495e1f1f3fd44a21294859d099c783433
StefDev1801 <stefano200118@gmail.com>
2024-02-16 15:31:35 +0100

    add Esercizio 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-document.writeln ('Hello word')
+//Esercizio 1: Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+
+const number = '100';
+
+document.writeln(number)
