6c9151ffd40654c871e710eb05149c0944d4d0de
StefDev1801 <stefano200118@gmail.com>
2024-02-16 16:07:35 +0100

    fix Esercizio 2 and add Esercizio 2/B

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,18 @@
 
 const number = '100'; //string
 
-console.log(number); //string?
+console.log(number) //string?
 
 //Esercizio 2: Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
 
-let PI = '3.14';
+const PI = '3.14'; //string
 
-console.log(PI);
+console.log( PI ) //string?
+
+//Esercizio  2/B: Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+
+PI = '3.1416'; //string
+
+console.log( PI ) 
+
+//Uncaught TypeError: Assignment to constant variable: questo il tipo di variabile non può essere cambiata dato che non è riassegnabile
